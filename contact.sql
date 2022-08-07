c79f296f49a46ed0e5cb8c97030d1db680825596
HsinYiChang <f1233772002@gmail.com>
2022-08-07 21:28:02 +0800

    0807修改

--- contact.sql
+++ contact.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2022-08-02 04:26:04
+-- 產生時間： 2022-08-07 08:14:35
 -- 伺服器版本： 10.4.24-MariaDB
 -- PHP 版本： 8.1.6
 
@@ -32,8 +32,17 @@ CREATE TABLE `user` (
   `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
   `sub` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `message` text COLLATE utf8mb4_unicode_ci NOT NULL
+  `message` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `sub`, `message`, `date`) VALUES
+(1, '欣儀', 'f1233772002@gmail.com', '你好', 'hello', '2022-08-07 06:13:50'),
+(2, '欣儀', 'f1233772002@gmail.com', 'hello', 'hihihi', '2022-08-07 06:14:09');
 
 --
 -- 已傾印資料表的索引
@@ -53,7 +62,7 @@ ALTER TABLE `user`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
