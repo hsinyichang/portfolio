b0b58e72d0e51c05f2bd3565337a42099a1d3d84
HsinYiChang <f1233772002@gmail.com>
2022-08-13 15:06:07 +0800

    完成新增修改頁尾版權

---
+++ api/edit.php
@@ -0,0 +1,25 @@
+<?php
+include_once "../base.php";
+$DB=new DB($_POST['table']);
+
+if(!empty($_POST['id'])){
+    foreach($_POST['id'] as $idx=>$id){
+        if(isset($_POST['del']) && in_array($id,$_POST['del'])){
+            $DB->del($id);
+        }else{
+            $row=$DB->find($id);
+            
+            switch($_POST['table']){
+                case 'r_footer':
+                    $row['footer']=$_POST['footer'][$idx]; //要加上key 才知道傳送的是第幾筆
+                    $row['sh']=(isset($_POST['sh']) && $_POST['sh']==$id)?1:0;
+                break;
+                
+            }
+
+            $DB->save($row);
+        }
+    }
+}
+
+to("../back.php?do={$_POST['table1']}");
---
+++ back/footer.php
@@ -0,0 +1,55 @@
+<style>
+    *{
+        box-sizing: border-box;
+    }
+    .footer_li{
+        display: flex;
+    }
+    .footer_div{
+        width: 60%;
+        margin: 2rem auto;
+    }
+    .footer_div div{
+        text-align: center;
+        margin-bottom: 1rem;
+    }
+</style>
+
+<div class="footer_div">
+    <form action="./api/add.php" method="post">
+        <li class="footer_li">
+            <div style="width: 10%;">名稱</div>
+            <div style="width: 70%;"><input type="text" name="footer"></div>
+            <div style="width: 10%;"><input type="submit" value="新增"></div>
+            <input type="hidden" name="table" value="r_<?=$do?>">
+            <input type="hidden" name="table1" value="<?=$do?>">
+        </li>
+    </form>
+    <form action="./api/edit.php" method="post">
+        <li class="footer_li">
+            <div style="width: 10%;">序號</div>
+            <div style="width: 70%;">頁尾名稱</div>
+            <div style="width: 10%;">顯示</div>
+            <div style="width: 10%;">刪除</div>
+        </li>
+        <?php
+        $rows=$Footer->all();
+        foreach($rows as $key=>$row){
+        ?>
+        <li class="footer_li">
+            <div style="width: 10%;"><?=$key+1?></div>
+            <div style="width: 70%;"><input type="text" name="footer[]" value="<?=$row['footer'];?>"></div>
+            <div style="width: 10%;"><input type="radio" name="sh" value="<?=$row['id']?>" <?=($row['sh']==1)?'checked':'';?>></div>
+            <div style="width: 10%;"><input type="checkbox" name="del[]" value="<?=$row['id']?>"></div>
+        </li>
+        <input type="hidden" name="id[]" value="<?=$row['id']?>">
+        <?php
+        }
+        ?>
+        <li class="footer_li">
+        <input type="submit" value="修改確定" style="margin:auto">
+        <input type="hidden" name="table" value="r_<?=$do?>">
+        <input type="hidden" name="table1" value="<?=$do?>">
+        </li>
+    </form>
+</div>
