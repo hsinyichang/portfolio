8f8df2ea4dc199d11ce7b90bf73798eb500f579d
M92561\user <ianyang@elifemail.com.tw>
2022-09-27 11:19:21 +0800

    刪除youtube

--- index.php
+++ index.php
@@ -552,8 +552,6 @@ include_once "base.php";
               </div> -->
               <div class="text-center"><button type="submit">Send Message</button></div>
             </form>
-            
-            <div class="col-lg-12"><iframe width="560" height="315" src="https://www.youtube.com/embed/_ibSYYXyfyY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
           
         </div>
 
